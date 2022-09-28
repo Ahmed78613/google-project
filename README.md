51fe37fbdcd5752459f5a6976d6fd7ec35723ab3
ahmed <ahmedmuhammed78613@gmail.com>
2022-09-28 16:43:07 +0100

    added new readme file

---
+++ README.md
@@ -0,0 +1,50 @@
+# Pet Search Engine
+
+Welcome to our Pet Search Engine!
+Search for the different types of pets and get back results which match your search.
+
+## Installation & Usage
+
+### Installation
+
+- Clone or download the repo.
+- Open terminal and navigate to the backend folder
+- Run `npm install` to install the dependencies
+
+### Usage
+
+- Open terminal and navigate to backend folder
+- Run `npm run dev` to ensure the server is up and running
+- Navigate to frontend folder and then and open the `index.html` with a live server
+
+## Technologies
+
+- JavaScript
+- Express
+- Node.js
+- Jest
+
+## Screenshots/Images
+
+![homepage](./frontend/images/main.png)
+![search results](./frontend/images/searchresults.png)
+
+## Bugs
+
+- When user Clicks on the random link button the api runs before redirecting to the page
+
+## Wins & Challenges
+
+### Wins
+
+- Developed our knowledge of how to fetch data
+- Learned how to remove dom elements using JavaScript
+
+### Challenges
+
+- Had issues integrating css and scss which set us back
+
+## Future Features
+
+- Frontend deployed on Firebase
+- Backend deployed on Heroku
--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,66 @@
-fetch()
+const searchBar = document.querySelector("#search");
+const searchBtn = document.querySelector("#searchButton");
+const randomBtn = document.querySelector("#randomButton");
+const form = document.querySelector("#searchBar");
+const searchResultsSection = document.querySelector("#searchResultsSection");
+const topSection = document.querySelector(".search-wrapper");
+const logo = document.querySelector(".logo");
+
+// Event Listeners
+form.addEventListener("submit", (e) => {
+	search(e);
+});
+randomBtn.addEventListener("click", () => {
+	searchRandom(searchBar.value);
+});
+
+async function search(e) {
+	e.preventDefault();
+	try {
+		// clear if any old results
+		const checkResults = document.querySelector(".myResults");
+		console.log(checkResults);
+		checkResults && checkResults.remove();
+		// get search query
+		const query = e.target.search.value;
+		// Fetch
+		const res = await fetch(`http://localhost:3000/${query}`);
+		const data = await res.json();
+		// Create new Div
+		const parentDiv = document.createElement("div");
+		parentDiv.classList.add("myResults");
+		data.map((result) => {
+			// Create new Elements
+			const div = document.createElement("div");
+			const aTag = document.createElement("a");
+			const pTag = document.createElement("p");
+			// Apply
+			aTag.textContent = result.title;
+			aTag.href = result.link;
+			pTag.textContent = result.description;
+			// Append
+			div.appendChild(aTag);
+			div.appendChild(pTag);
+			parentDiv.appendChild(div);
+		});
+		searchResultsSection.appendChild(parentDiv);
+		// Styling
+		topSection.style.flexDirection = "row";
+		logo.style.marginRight = "30px";
+	} catch (error) {
+		window.alert("No results for query, please try again.");
+	}
+}
+
+// I'm Feeling Lucky Button
+async function searchRandom(query) {
+	try {
+		// Fetch
+		const res = await fetch(`http://localhost:3000/${query}/random`);
+		const data = await res.json();
+		// Redirect
+		location.href = data.link;
+	} catch (error) {
+		window.alert("No results for querys, please try again.");
+	}
+}
